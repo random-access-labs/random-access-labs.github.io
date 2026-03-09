c2615e84d2c3e5fbc427a2c53e3c900b027ca7dc
Jubin Savla <jubinmr@gmail.com>
2026-03-09 21:50:20 +1100

    Adding a duplicate folder named "open" than "open-app" to support sylo version 1.2.7

---
+++ open/config.js
@@ -0,0 +1,20 @@
+// Add new apps to Random Access Labs here
+const APP_CONFIGS = {
+    "sylo-game": {
+        name: "SYLO",
+        iosStore: "https://apps.apple.com/us/app/sylo/id6759213888",
+        androidStore: "https://play.google.com/store/apps/details?id=com.randomaccesslabs.sylo",
+        deepLink: "sylo-game://",
+        webFallback: "https://random-access-labs.github.io/#current-production"
+    },
+    qrido: {
+        name: "QRido",
+        iosStore: null, // Update once launched on App Store
+        androidStore: null, // Update once launched on Play Store
+        deepLink: "qrido://",
+        webFallback: "https://qrido.bharticomputer.in"
+    }
+};
+
+// Export if using modules, otherwise it sits in global scope for index.html
+if (typeof module !== 'undefined') { module.exports = APP_CONFIGS; }
